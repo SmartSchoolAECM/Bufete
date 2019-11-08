c88cb7565c011e8f101935bd490de97d1b9b4997
unknown <utilizador@ESCM-S119-PC06.AECASTELO.LOCAL>
2019-11-08 12:12:42 +0000

    Pagina Startup feita e design do login tambem
    Falta tentar mexer nos TextField para terem melhor tamanho

---
+++ BufeteApp/www/scripts/login.js
@@ -0,0 +1,4 @@
+﻿//iniciar textfield
+mdc.textField.MDCTextField.attachTo(document.querySelector('#UsernameBig'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('#PasswordBig'));
+
---
+++ BufeteApp/www/scripts/startup.js
@@ -0,0 +1,8 @@
+﻿$('#Login, #Registo').click(function () {
+    if (this.id == 'Login') {
+        window.location = "../pages/login.html"
+    }
+    else if (this.id == 'Registo') {
+        window.location = "../pages/registo.html"
+    }
+});
